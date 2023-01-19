c34e5ad265580b0a6c11e9b8209c1a5d6ee221e6
webdevFaizan <webdev.faizan@gmail.com>
2023-01-20 04:28:17 +0530

    The first basic react-native components build and it starts working. The different components are being loaded.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native";
+import Home from "./screens/Home";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <Home/>
     </View>
   );
 }
