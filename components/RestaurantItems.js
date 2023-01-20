b7eed4d0f09a057772745d99309ee68239cd6f94
webdevFaizan <webdev.faizan@gmail.com>
2023-01-21 01:52:07 +0530

    Added food items to list.

--- components/RestaurantItems.js
+++ components/RestaurantItems.js
@@ -1,37 +1,57 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-export default function RestaurantItems() {
+export default function RestaurantItems(props) {
   return (
     <View style={{paddingHorizontal : 10, marginTop : 10}}>
       {/* <Text>RestaurantItems</Text> */}
-      <RestaurantImage/>
+      <RestaurantImage uri={props.uri}/>
       <RestaurantText/>
     </View>
   )
 }
 
-const RestaurantImage = ()=>{
+const RestaurantImage = (props)=>{
     return (
         <View>
             <Image 
-            source={{uri : "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+            source={{uri : props.uri, 
             width : "100%", 
             height : 200, }}/>
             {/* <Text>Hello</Text> */}
+            <TouchableOpacity style={{position : 'absolute', top: 5, right:5}}>
+                <MaterialCommunityIcons name='heart-outline' size={39} color='white'/>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const RestaurantText =()=>{
     return (
-        <View style={{flexDirection : 'row', justifyContent: 'space-between'}}>
+        <View
+            style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: 10,
+            }}
+        >
             <View style={{flexDirection : 'column'}}>
-                <Text>Meal Name</Text>
-                <Text>Delivery Time</Text>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>Extra Cheeze Margretta Pizza</Text>
+                <Text style={{ fontSize: 13, color: "gray" }}>30-45 • min</Text>
             </View>
-            <View>
-                <Text>Rating</Text>
+            <View
+            style={{
+                backgroundColor: "#eee",
+                height: 30,
+                width: 30,
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 15,
+            }}
+            >
+                <Text>4.5</Text>
             </View>            
         </View>
     )
--- screens/Home.js
+++ screens/Home.js
@@ -13,10 +13,16 @@ export default function Home() {
         <SearchBar/>       
       </View>
         <Categories/>
-      <ScrollView>
-        <RestaurantItems/>
-        <RestaurantItems/>
-        <RestaurantItems/>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <RestaurantItems uri="https://as1.ftcdn.net/v2/jpg/01/27/65/58/1000_F_127655811_iv0Vf94XxAy7hneGXka6C1kaT8jAe4qC.jpg"/>
+        <RestaurantItems uri="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <RestaurantItems uri="https://as1.ftcdn.net/v2/jpg/01/12/25/74/1000_F_112257403_IE7hN2cUR6W9PZvSQAwDOUUT7S7fbeGk.jpg"/>
+        <RestaurantItems uri="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <RestaurantItems uri="https://as1.ftcdn.net/v2/jpg/01/12/25/74/1000_F_112257403_IE7hN2cUR6W9PZvSQAwDOUUT7S7fbeGk.jpg"/>
+        <RestaurantItems uri="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        <RestaurantItems uri="https://as1.ftcdn.net/v2/jpg/01/27/65/58/1000_F_127655811_iv0Vf94XxAy7hneGXka6C1kaT8jAe4qC.jpg"/>
+        <RestaurantItems uri="https://as1.ftcdn.net/v2/jpg/01/12/25/74/1000_F_112257403_IE7hN2cUR6W9PZvSQAwDOUUT7S7fbeGk.jpg"/>
+        <RestaurantItems uri="https://as1.ftcdn.net/v2/jpg/01/27/65/58/1000_F_127655811_iv0Vf94XxAy7hneGXka6C1kaT8jAe4qC.jpg"/>        
       </ScrollView>
     </>
   )
