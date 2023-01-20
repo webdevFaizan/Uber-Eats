2ab6a7b942e83e61c1c28597cf22547013adc6a5
webdevFaizan <webdev.faizan@gmail.com>
2023-01-21 02:18:25 +0530

    Hard coded restaurant data is now passable in the component, we will later on call the yelp api and display the data.

--- components/RestaurantItems.js
+++ components/RestaurantItems.js
@@ -2,13 +2,70 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
+export const localRestaurants = [
+    {
+      name: "Beachside Bar",
+      image_url:
+        "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+      categories: ["Cafe", "Bar"],
+      price: "$$",
+      reviews: 1244,
+      rating: 4.5,
+    },
+    {
+      name: "Benihana",
+      image_url:
+        "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      categories: ["Cafe", "Bar"],
+      price: "$$",
+      reviews: 1244,
+      rating: 3.7,
+    },
+    {
+      name: "India's Grill",
+      image_url:
+        "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      categories: ["Indian", "Bar"],
+      price: "$$",
+      reviews: 700,
+      rating: 4.9,
+    },    
+    {
+      name: "India's Grill",
+      image_url:
+        "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      categories: ["Indian", "Bar"],
+      price: "$$",
+      reviews: 700,
+      rating: 4.9,
+    },
+    {
+      name: "India's Grill",
+      image_url:
+        "https://as1.ftcdn.net/v2/jpg/01/12/25/74/1000_F_112257403_IE7hN2cUR6W9PZvSQAwDOUUT7S7fbeGk.jpg",
+      categories: ["Indian", "Bar"],
+      price: "$$",
+      reviews: 700,
+      rating: 4.9,
+    },
+  ];
+
+
+
 export default function RestaurantItems(props) {
   return (
-    <View style={{paddingHorizontal : 10, marginTop : 10}}>
-      {/* <Text>RestaurantItems</Text> */}
-      <RestaurantImage uri={props.uri}/>
-      <RestaurantText/>
-    </View>
+    <TouchableOpacity activeOpacity={1} style={{marginBottom :10}}>
+        {
+            localRestaurants.map((restaurant, index)=>(
+                <View style={{padding : 10, marginTop : 10, backgroundColor : 'white'}}>
+                    {/* <Text>RestaurantItems</Text> */}
+                    <RestaurantImage uri={restaurant.image_url}/>
+                    <RestaurantText name={restaurant.name} rating={restaurant.rating}/>
+                </View>
+            ))
+        }
+
+    </TouchableOpacity>
   )
 }
 
@@ -27,7 +84,7 @@ const RestaurantImage = (props)=>{
     )
 }
 
-const RestaurantText =()=>{
+const RestaurantText =(props)=>{
     return (
         <View
             style={{
@@ -38,7 +95,7 @@ const RestaurantText =()=>{
             }}
         >
             <View style={{flexDirection : 'column'}}>
-                <Text style={{ fontSize: 15, fontWeight: "bold" }}>Extra Cheeze Margretta Pizza</Text>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
                 <Text style={{ fontSize: 13, color: "gray" }}>30-45 • min</Text>
             </View>
             <View
