c28e00d99de7a3a2311da0b891e519f4078796ab
webdevFaizan <webdev.faizan@gmail.com>
2023-01-22 16:37:22 +0530

    The whole components folder structure is changed since we are going to introduce some other global component sections that would be helpful for other functionality like when we click on the restaurant image we should be able to enter in the menu that restaurant is offering.

--- components/home/Categories.js
+++ components/home/Categories.js
@@ -3,37 +3,37 @@ import React from 'react';
 
 const items = [
   {
-    image: require("../assets/images/shopping-bag.png"),
+    image: require("../../assets/images/shopping-bag.png"),
     text: "Pick-up",
     id : "1"
   },
   {
-    image: require("../assets/images/soft-drink.png"),
+    image: require("../../assets/images/soft-drink.png"),
     text: "Soft Drinks",
     id : "2"
   },
   {
-    image: require("../assets/images/bread.png"),
+    image: require("../../assets/images/bread.png"),
     text: "Bakery Items",
     id : "3"
   },
   {
-    image: require("../assets/images/fast-food.png"),
+    image: require("../../assets/images/fast-food.png"),
     text: "Fast Foods",
     id : "4"
   },
   {
-    image: require("../assets/images/deals.png"),
+    image: require("../../assets/images/deals.png"),
     text: "Deals",
     id : "5"
   },
   {
-    image: require("../assets/images/coffee.png"),
+    image: require("../../assets/images/coffee.png"),
     text: "Coffee & Tea",
     id : "6"
   },
   {
-    image: require("../assets/images/desserts.png"),
+    image: require("../../assets/images/desserts.png"),
     text: "Desserts",
     id : "7"
   },
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,10 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import HeaderTabs from '../components/HeaderTabs'
-import SearchBar from '../components/SearchBar'
-import Categories from '../components/Categories'
-import RestaurantItems , {localRestaurants} from '../components/RestaurantItems'
-import BottomTabs from '../components/BottomTabs'
+import HeaderTabs from '../components/home/HeaderTabs'
+import SearchBar from '../components/home/SearchBar'
+import Categories from '../components/home/Categories'
+import RestaurantItems , {localRestaurants} from '../components/home/RestaurantItems'
+import BottomTabs from '../components/home/BottomTabs'
 // import { Divider } from 'react-native-elements';
 
 const YELP_API_KEY =  "zeB0Ha-IuKHERItBz_PmW9VJoQWnUpUeTgeaNiwDIs8oQS6-bgf3rELGkKFfecSw5Oh-AaNWN18IU41k_JDQMVCVC8-6cpk03ReXTpwOGZZ1HCGX094WAPycbWHMY3Yx";
@@ -24,16 +24,14 @@ export default function Home(){
     return fetch(yelpURI, apiOptions).then(response=>response.json()).then(json=> {
       console.log(json.businesses);      
       //IMPORTANT : The following use case is the best example of using the filter function, which will filter all the data that consists of activeTab that has either Delivery or Pickup related information, it will give the complete data of all the restaurants that it picks, but it will only select filtered list of restaurant.
-        setRestaurantData(
-          json.businesses.filter((business) =>
-            business.transactions.includes(activeTab.toLowerCase()))
-        )
+        // setRestaurantData(json.businesses.filter((business) =>business.transactions.includes(activeTab.toLowerCase())) //IMPORTANT : The api has changed, this filter function was able to filter the data between the pickup and delivery restaurants. But in the new api response, we are receiving an empty transactions array, this is why we are not able to filter the data sent by the api, now this does not mean that we are calling the api wrong way but it means that docs can change. And we will not be able to switch between pickup and delivery restaurants.
+        setRestaurantData(json.businesses);        
       }
     ).catch(err=>console.log(err));
   }
 
   useEffect(()=>{
-    // restaurantFromYelp();    //When the API key limit is reached, this means that we cannot call the API using this key anymore.
+    restaurantFromYelp();    //When the API key limit is reached, this means that we cannot call the API using this key anymore.
   },[city,activeTab]);    //IMPORTANT : This city variable needs to be tracked, since we are looking for the change in the state of city and as soon as it is done, this useEffect will fire the inner function, so that the new results could be shown.
   // This activeTab will decide whether the data is going to be related to the Delivery Restaurant or related to the pickup restaurant.
   return (
