3a278c2150113d37acccad6ae025c73055a938b9
unknown <webdev.faizan@gmail.com>
2023-01-25 22:54:19 +0530

    If the total value of items in the cart is 0, then this will not let the ViewCart render, and as soon as an item is added the cart is rendered.

--- components/restaurantDetails/ViewCart.js
+++ components/restaurantDetails/ViewCart.js
@@ -36,45 +36,47 @@ import { useSelector } from 'react-redux'
 // }
 
 export default function ViewCart() {
-    const items = useSelector((state)=>state.cartReducer.selectedItems.items);
-    // console.log(items);
-    const priceArray = items.map((item)=>Number(item.price.replace('$','')));
-    let totalPrice = priceArray.reduce((prev, curr)=>prev+curr,0);
+    // IMPORTANT : We are using the useSelector hook to extract the value of the items from the cartReducer, so that we could use it in this component.
+    const items = useSelector((state)=>state.cartReducer.selectedItems.items);    
+
+    // Once the items has been extracted from the global store then the price of each item is extracted, converted to number, $ sign removed, then added to give us the final value so that it could be viewed in the ViewCart component.
+    let totalPrice = items.map((item)=>Number(item.price.replace('$',''))).reduce((prev, curr)=>prev+curr,0);
     console.log(totalPrice);
 
-    // let totalPrice = items.reduce()
-
-    return (    
-        <View style={{
-            // IMPORTANT : This view cart component had to be kept on the top of all the other elements, and the position had to be defined in respect to the whole screen, this is why flex =1 had to be kept, and the position of this component had to be absolute, so that no matter the scrolling component, the view cart remained at its designated place.
-            flex : 1,
-            alignItems : 'center',            
-            flexDirection : 'row',
-            position : 'absolute',
-            bottom : 30,
-            zIndex : 897            
-        }}>
+    return (
+        <>{totalPrice==0?'':(
             <View style={{
-                width : '100%',     //This will make the width of the container occupy the 100% of the width from the parent container, but at the same time justifyContent will make it arranged to center.
+                // IMPORTANT : This view cart component had to be kept on the top of all the other elements, and the position had to be defined in respect to the whole screen, this is why flex =1 had to be kept, and the position of this component had to be absolute, so that no matter the scrolling component, the view cart remained at its designated place.
+                flex : 1,
+                alignItems : 'center',            
                 flexDirection : 'row',
-                justifyContent : 'center',
-            }}
-
-            >
-                {/* IMPORTANT : Do not think of TouchableOpacity as a separate component, it is like a customised button, this is why styling could help it in giving the required shape and sizes. */}
-                <TouchableOpacity style={{
-                    // marginTop : 20,
-                    backgroundColor : 'black',
-                    alignItems : 'center',
-                    padding : 13,
-                    borderRadius : 30,
-                    width : 250,
-                    position : 'relative'
+                position : 'absolute',
+                bottom : 30,
+                zIndex : 897            
+            }}>
+                <View style={{
+                    width : '100%',     //This will make the width of the container occupy the 100% of the width from the parent container, but at the same time justifyContent will make it arranged to center.
+                    flexDirection : 'row',
+                    justifyContent : 'center',
                 }}
+
                 >
-                    <Text style={{color : 'white', fontSize : 20}}>VIEWCART {'$'+totalPrice}</Text>
-                </TouchableOpacity>      
-            </View>  
-        </View>
+                    {/* IMPORTANT : Do not think of TouchableOpacity as a separate component, it is like a customised button, this is why styling could help it in giving the required shape and sizes. */}
+                    <TouchableOpacity style={{
+                        // marginTop : 20,
+                        backgroundColor : 'black',
+                        alignItems : 'center',
+                        padding : 13,
+                        borderRadius : 30,
+                        width : 250,
+                        position : 'relative'
+                    }}
+                    >
+                        <Text style={{color : 'white', fontSize : 20}}>VIEWCART {'$'+totalPrice}</Text>
+                    </TouchableOpacity>      
+                </View>  
+            </View>)
+        }
+        </>
     )
   }
